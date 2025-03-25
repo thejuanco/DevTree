3a7c3a57812e453759a4fd7121d3b36f08b6b140
Juanco <juanffo874@gmail.com>
2025-03-25 08:26:50 -0600

    Habilita la conexion a la base de datos

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import colors from 'colors';    
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect("")
+        const {connection} = await mongoose.connect(process.env.DATABASE_MONGO)
+        const url = `${connection.host}:${connection.port}/${connection.name}`
+        console.log(`Conectado a la base de datos: ${url}`);
     } catch (error) {
-        console.log(error);
+        console.log(colors.bgRed.white.bold(error));
         //Terminar la ejecucion de la aplicacion
         process.exit(1);
     }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 //Esta archivo solo sirve para arrancar el servidor
 import express from "express";
-import router from "./routes/Router.routes"
+import router from "./routes/routes";
+import "dotenv/config";
+import { connectDB } from "./config/db";
 
 const app = express();
+
+//Conectar a la base de datos
+connectDB();
 
 //Habilitar la lectura de formularios
 app.use(express.json());
