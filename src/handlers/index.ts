af8bc0f6296b6d4c1389e97ab7c441ff6a294016
Juanco <juanffo874@gmail.com>
2025-03-30 20:20:45 -0600

    Hashea la constraseña para que nose visible

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,27 @@
 import type { Request, Response } from "express" // Importing Request and Response types from express
 import UserModel from "../models/User"
+import { hashPassword } from "../untils/auth" 
 
-export const createAccount = async (req : Request , res : Response ) => {
-    await UserModel.create(req.body)
+export const createAccount = async (req : Request , res : Response ): Promise<void> => {
+    try {
+        // Check if user already exists
+        const {email, password} = req.body
+        const userExists = await UserModel.findOne({email})
 
-    res.json({message: "User created"})
+        if(userExists){
+            const error = new Error("El usuario ya esta registrado")
+            res.status(409).json({error: error.message})
+            return
+        }
+
+        const user = new UserModel(req.body)
+        //Hashear la contraseña
+        user.password = await hashPassword(password)
+
+        await user.save()
+
+        res.json({message: "User created"})
+    } catch (error) {
+        console.log(error)
+    }
 }
