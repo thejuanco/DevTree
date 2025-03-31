3e7580f6caf4280ef8093cd9178f5fb0712033ee
Juanco <juanffo874@gmail.com>
2025-03-30 18:07:01 -0600

    Añade types al Routing de express

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,7 @@
+import type { Request, Response } from "express" // Importing Request and Response types from express
 import UserModel from "../models/User"
 
-export const createAccount = async (req, res) => {
+export const createAccount = async (req : Request , res : Response ) => {
     await UserModel.create(req.body)
 
     res.json({message: "User created"})
