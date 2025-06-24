687beb12fc6ad14c729a2d28a61f0cccf9f0da63
Juanco <juanffo874@gmail.com>
2025-06-24 13:16:05 -0600

    Añade configuración de CORS y actualiza dependencias en package.json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 //Esta archivo solo sirve para arrancar el servidor
 import express from "express";
+import cors from "cors"
 import router from "./routes/routes";
 import "dotenv/config";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
 
 const app = express();
 
 //Conectar a la base de datos
 connectDB();
+
+//cors
+app.use(cors(corsConfig))
 
 //Habilitar la lectura de formularios
 app.use(express.json());
