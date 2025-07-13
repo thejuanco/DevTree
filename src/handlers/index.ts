0a00fdf4b6a51aefcf478e522ea7a3be0ddcb01f
Juanco <juanffo874@gmail.com>
2025-07-12 22:31:49 -0600

    Indetifica al usuario por medio del JWT

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express" // Importing Request and Response types from express
 import { validationResult } from "express-validator"
 import slug from "slug"
+import jwt from "jsonwebtoken"
 import UserModel from "../models/User"
 import { hashPassword, checkPassword } from "../untils/auth" 
 import { generateJWT } from "../untils/jwt"
@@ -64,5 +65,40 @@ export const login = async (req : Request, res: Response ) : Promise<void> => {
         res.send(token)
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const getUser = async (req: Request, res: Response ) => {
+    const bearer = req.headers.authorization
+
+    //Validacion
+    if(!bearer){
+        const error = new Error('No autorizado')
+        res.status(401).json({error: error.message})
+        return
+    }
+
+    //toma la parte de la derecha y la asigna a token
+    const [, token] = bearer.split(' ')
+
+    if(!token){
+        const error = new Error('No autorizado')
+        res.status(401).json({error: error.message})
+        return
+    }
+
+    try {
+        const result = jwt.verify(token, process.env.JWT_SECRET)
+        if(typeof result === "object" && result.id){
+            const user = await UserModel.findById(result.id).select('name handle email')
+            if(!token){
+                const error = new Error('El usuario no existe')
+                res.status(404).json({error: error.message})
+                return
+            }
+            res.json(user)
+        }
+    } catch (error) {
+        res.status(500).json({error: "Token no válido"})
     }
 }
