668da6a3f8a8bce710793ecb75cc3cbacce07906
Juanco <juanffo874@gmail.com>
2025-08-27 23:10:01 -0600

    Agrega las credenciales de Cloudinary al server

--- src/models/User.ts
+++ src/models/User.ts
@@ -33,6 +33,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
         trim: true
+    },
+    description: {
+        type: String,
+        default: ""
     }
 })
 
